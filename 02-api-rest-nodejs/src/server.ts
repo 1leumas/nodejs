b82d98cb5e678a93cb003a299069bffc56823a79
Samuel <133022102+1leumas@users.noreply.github.com>
2024-09-15 13:40:42 -0300

    banco de dados

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,14 +1,23 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import { randomUUID } from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  const transaction = await knex('transactions').insert({
+    id: randomUUID(),
+    title: 'Salary',
+    amount: 1000,
+  })
+
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
